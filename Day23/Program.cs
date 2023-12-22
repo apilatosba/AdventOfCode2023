afe07d6cf4601e7e9ac749ee8db7820f90a80308
apilatosba <aposdapulos098@outlook.com>
2023-12-22 10:13:54 +0300

    pre initialzizeaz

---
+++ Day23/Program.cs
@@ -0,0 +1,13 @@
+namespace Day23 {
+   class Program {
+      static void Main() {
+         Part1();
+      }
+
+      static void Part1() {
+      }
+
+      static void Part2() {
+      }
+   }
+}
